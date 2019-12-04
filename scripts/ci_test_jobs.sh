e5369258fd2a9ed1cc1ac1bfde80ed82cf9148c9
Archit Aggarwal <architag@amazon.com>
2019-12-04 11:12:29 -0800

    Revert "Comment out more commands in Jobs CI script"
    This reverts commit 10d586d129137ef929432498f98e5dde35207372.

--- scripts/ci_test_jobs.sh
+++ scripts/ci_test_jobs.sh
@@ -58,8 +58,8 @@ run_tests
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then delete_jobs; fi
 
 # Rebuild in static memory mode.
-# cmake .. -DIOT_BUILD_TESTS=1 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS="$CMAKE_FLAGS -DIOT_STATIC_MEMORY_ONLY=1"
-# make -j2 aws_iot_tests_jobs
+cmake .. -DIOT_BUILD_TESTS=1 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS="$CMAKE_FLAGS -DIOT_STATIC_MEMORY_ONLY=1"
+make -j2 aws_iot_tests_jobs
 
 # Run tests in static memory mode.
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then create_jobs; fi
