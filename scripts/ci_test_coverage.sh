f470f10066fbcd3b90f1f3e12bcdea5907f300d3
Archit Aggarwal <architag@amazon.com>
2019-12-04 15:30:01 -0800

    Update coverage script to generate coverage separately for each library

--- scripts/ci_test_coverage.sh
+++ scripts/ci_test_coverage.sh
@@ -6,6 +6,15 @@
 # Exit on any nonzero return code.
 set -ev
 
+generate_coverage(){
+    # Generate code coverage results, but only for files in libraries/.
+    lcov --directory . --capture --output-file $1
+    lcov --remove $1 '*demo*' --output-file $1
+    lcov --remove $1 '*ports*' --output-file $1
+    lcov --remove $1 '*tests*' --output-file $1
+    lcov --remove $1 '*third_party*' --output-file $1
+}
+
 # Overwrite the value of the COMPILER_OPTIONS varirable to remove any thread sanitizer flags, and replace with coverage flags.
 export COMPILER_OPTIONS="-DIOT_TEST_COVERAGE=1 --coverage"
 
@@ -13,25 +22,26 @@ SCRIPTS_FOLDER_PATH=../scripts
 
 # Run common tests with code coverage.
 $SCRIPTS_FOLDER_PATH/ci_test_common.sh
+generate_coverage common.info
 
 # Run MQTT tests against AWS IoT with code coverage.
 $SCRIPTS_FOLDER_PATH/ci_test_mqtt.sh
+generate_coverage mqtt.info
 
 # Run Shadow tests with code coverage.
 $SCRIPTS_FOLDER_PATH/ci_test_shadow.sh
+generate_coverage shadow.info
 
 # Run Jobs tests with code coverage.
 $SCRIPTS_FOLDER_PATH/ci_test_jobs.sh
+generate_coverage jobs.info
 
 # Run Provisioning tests with code coverage.
-$SCRIPTS_FOLDER_PATH/ci_test_provisioning.sh
+# $SCRIPTS_FOLDER_PATH/ci_test_provisioning.sh
+# generate_coverage provisioning.info
 
-# Generate code coverage results, but only for files in libraries/.
-lcov --directory . --capture --output-file coverage.info
-lcov --remove coverage.info '*demo*' --output-file coverage.info
-lcov --remove coverage.info '*ports*' --output-file coverage.info
-lcov --remove coverage.info '*tests*' --output-file coverage.info
-lcov --remove coverage.info '*third_party*' --output-file coverage.info
+# Combine all the coverage reports of all libraries into a single file.
+lcov -a common.info -a mqtt.info -a shadow.info -a jobs.info -o coverage.info  
 
 # Submit the code coverage results. Must be submitted from SDK root directory so
 # Coveralls displays the correct paths.
