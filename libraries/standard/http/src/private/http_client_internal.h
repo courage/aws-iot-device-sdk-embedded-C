3c6d0a3edd71a754bdfcfb0c9ef2169fc569a87f
Archit Aggarwal <architag@amazon.com>
2020-04-22 23:15:42 +0000

    Update internal headers to fix config.h dependency

--- libraries/standard/http/src/private/http_client_internal.h
+++ libraries/standard/http/src/private/http_client_internal.h
@@ -1,18 +1,12 @@
 #ifndef __HTTP_CLIENT_INTERNAL_H__
 #define __HTTP_CLIENT_INTERNAL_H__
 
-#include "config.h"
-
 #ifdef USE_LOGGING_FRAMEWORK
-    #ifdef HTTP_LOG_LEVEL
-        #define LIBRARY_LOG_LEVEL        MQTT_LOG_LEVEL
-    #else
-        #ifdef IOT_LOG_LEVEL_GLOBAL
-            #define LIBRARY_LOG_LEVEL    IOT_LOG_LEVEL_GLOBAL
-        #else
-            #define LIBRARY_LOG_LEVEL    IOT_LOG_NONE
-        #endif
-    #endif
+    #define LIBRARY_LOG_LEVEL    IOT_LOG_INFO
+
+    #define LIBRARY_LOG_NAME     "HTTP"
 #endif /* ifdef USE_LOGGING_FRAMEWORK */
+
+#include "config.h"
 
 #endif /* ifndef __HTTP_CLIENT_INTERNAL_H__ */
--- libraries/standard/mqtt/src/private/mqtt_client_internal.h
+++ libraries/standard/mqtt/src/private/mqtt_client_internal.h
@@ -1,18 +1,14 @@
 #ifndef __MQTT_CLIENT_INTERNAL_H__
 #define __MQTT_CLIENT_INTERNAL_H__
 
-#include "config.h"
-
 #ifdef USE_LOGGING_FRAMEWORK
-    #ifdef MQTT_LOG_LEVEL
-        #define LIBRARY_LOG_LEVEL        MQTT_LOG_LEVEL
-    #else
-        #ifdef IOT_LOG_LEVEL_GLOBAL
-            #define LIBRARY_LOG_LEVEL    IOT_LOG_LEVEL_GLOBAL
-        #else
-            #define LIBRARY_LOG_LEVEL    IOT_LOG_NONE
-        #endif
-    #endif
-#endif
+
+    #define LIBRARY_LOG_NAME     "MQTT"
+
+    #define LIBRARY_LOG_LEVEL    IOT_LOG_ERROR
+
+#endif /* ifdef USE_LOGGING_FRAMEWORK */
+
+#include "config.h"
 
 #endif /* ifndef __MQTT_CLIENT_INTERNAL_H__ */
