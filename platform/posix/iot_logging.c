f0271ed5cf27d6a9c365fbf56bea3b5c3ad8fe7e
Archit Aggarwal <architag@amazon.com>
2020-04-23 22:31:48 +0000

    Missed C90 functions implementation

--- platform/posix/iot_logging.c
+++ platform/posix/iot_logging.c
@@ -127,6 +127,36 @@ static bool _reallocLoggingBuffer( void ** pOldBuffer,
 
 /*-----------------------------------------------------------*/
 
+void IotLog_Error( const char * const pFormat,
+                   ... )
+{
+    puts( "C90 Error\n" );
+}
+
+/*-----------------------------------------------------------*/
+void IotLog_Warn( const char * const pFormat,
+                  ... )
+{
+    puts( "C90 Warn\n" );
+}
+
+/*-----------------------------------------------------------*/
+void IotLog_Info( const char * const pFormat,
+                  ... )
+{
+    puts( "C90 Info\n" );
+}
+
+/*-----------------------------------------------------------*/
+
+void IotLog_Debug( const char * const pFormat,
+                   ... )
+{
+    puts( "C90 Debug\n" );
+}
+
+/*-----------------------------------------------------------*/
+
 void IotLog_Generic( int32_t messageLevel,
                      const char * const pFormat,
                      ... )
